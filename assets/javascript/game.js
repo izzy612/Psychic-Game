17051c73cc7cd1a900fcdc3306e4e47873085d13
Isabella Torres <izzy612@gmail.com>
2019-03-28 23:32:10 -0400

    finished game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,6 @@
 
+//Setting variables
+
 var correct = 0;
 var incorrect = 0;
 var guessesLeft = 9;
@@ -13,6 +15,8 @@ var alphabet = [
 			];
 
 
+      
+//Generates random number
 
 var pyschic = function()  {
 	pyschicChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
@@ -21,6 +25,7 @@ var pyschic = function()  {
 
 }
 
+//function will reset everything after win or lose
 
 var resetGame = function() {
 	guessesLeft = 9;
@@ -29,8 +34,8 @@ var resetGame = function() {
 };
 
 
-
-
+// captures the players input
+//.onkeyup captures the players input as userGuess
 
 document.onkeyup = function(event)  {
 
@@ -38,13 +43,15 @@ document.onkeyup = function(event)  {
 
 	pyschic();
 
-	
-
+  //handles correct guesses	
 
 	if (userGuess == pyschicChoice) {
 		correct++;
 		alertWin();
-	} 
+  } 
+  
+  // handles incorrect guesses
+
 	if (userGuess != pyschicChoice) {
 		guessesLeft--;
     guessesSoFar.push(userGuess);
@@ -54,7 +61,7 @@ document.onkeyup = function(event)  {
 	// Output to the webpage
 	var html = 
 		"<p>Guesses Left </p>" +
-		"<p>${guessesLeft}</p>" +
+		"<p>"+ guessesLeft + "</p>" +
 		"<p>Letters Guessed</p>" + 
 		"<p>"+ guessesSoFar + "</p>" +
 		"<br>" +
@@ -65,7 +72,9 @@ document.onkeyup = function(event)  {
 
 	// Set HTML contents of the game_output id to our html string
     document.querySelector("#game_output").innerHTML = html;
-
+    
+    // When the user is out of guesses, an alert will show up  
+    
     if ( guessesLeft === 0 ) {
 		incorrect++;
 		alertLoss();
@@ -73,11 +82,13 @@ document.onkeyup = function(event)  {
 	}
 
 };
-
+// An alert of winning the game and the game will reset
 var alertWin = function() {
 	alert("Congratulations You Won ");
 	resetGame();
 };
+
+// An alert of losing the game and the game will reset
 
 var alertLoss = function() {
 	alert("I am sorry you are incorrect, I was thinking of " + pyschicChoice + ". " + "You Guessed " + userGuess + ".");
